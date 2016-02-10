6ad5aa384fcd2337749159d169712526e8bf9e03
administrateur <administrateur@L-Latitude3550.adn.fr>
2016-02-10 16:20:32 +0100

    error in main

--- src/login/Login.java
+++ src/login/Login.java
@@ -24,7 +24,7 @@ import java.awt.EventQueue;
     /**
      * Launch the application.
      */
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
